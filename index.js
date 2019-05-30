9680f4ec6ae6a03185245442f520090a90e8b595
Philip Roth <phil.roth077@gmail.com>
2019-05-30 23:35:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 var recipes = {}
 
-var updateObjectWithKeyAndValue = {object, key, value}
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value})
+}
